f7d35df5d478217cc3d6a378ffbe6f26c5fbb72f
Dale <dburke2@ggc.edu>
2016-02-24 04:20:33 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require './lib/quiz.rb'
+set :public_folder, "public"
 
 enable :sessions
 
 get '/' do
+  session.clear
   erb :index
 end
 
@@ -12,7 +14,8 @@ get '/question' do
   if session['question'] 
     # out of questions, present score
     if session['question'] > Quiz::QUESTION_LIST.length - 1
-      return erb :score
+      return erb :score, :locals => { :score => (session['correct'].to_f / \
+      Quiz::QUESTION_LIST.length.to_f * 100).round(2) }
     end
     current = session['question']
   else
@@ -28,7 +31,8 @@ post '/question' do
   choice = params['choice']
   result = false
   if question.answer == question.choices[choice.to_i]
-     result = true
+    result = true
+    session['correct'] = session['correct'].to_i + 1
   end  
   session['question'] = session['question'] + 1
   erb :result, :locals => { :result => result }
--- lib/quiz.rb
+++ lib/quiz.rb
@@ -7,7 +7,13 @@ module Quiz
   Q2 = Question.new('Who is/was chief justice in 2015?','Roberts',['Scalia','Kennedy','Roberts','Satomayor'])
 
   Q3 = Question.new('What is the state capital of Georgia?','Atlanta',['Lawrenceville','Suwanee','Duluth','Atlanta'])
+  
+  Q4 = Question.new('Which individual was inaugurated as president after losing the popular vote?','John Quincy Adams',['Ronald Reagan','Richard Nixon','John Quincy Adams','Bill Clinton'])
+  
+  Q5 = Question.new('Who served as mayor of Lawrenceville, GA in 2015?','Judy Jordan Johnson',['Nathan Deal','Judy Jordan Johnson','Ethan Patterson','Barack Obama'])
+  
+  Q6 = Question.new('Approximately how high was the US national debt in 2015?','$18 trillion',['$18 million','$1.8 billion','$1.8 trillion','$18 trillion'])
 
-  QUESTION_LIST = [Q1, Q2, Q3]
+  QUESTION_LIST = [Q1, Q2, Q3, Q4, Q5, Q6]
 
 end
